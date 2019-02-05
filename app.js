9b020b543d77b86fd5258e682b8101c9893f0093
Rachel Hofer <rhofer3@gmail.com>
2019-02-05 18:20:42 -0500

    vulnerability removed

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
 
+const cors = require('cors');
 // const session       = require('express-session');
 // const passport      = require('passport');
 
@@ -24,6 +25,7 @@ mongoose
     console.error('Error connecting to mongo', err)
   });
 
+
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
@@ -35,8 +37,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// Express View engine setup
 
+// Express View engine setup
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
@@ -49,12 +51,13 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+
 // ADD SESSION SETTINGS HERE:
-app.use(session({
-  secret: "some secret goes here",
-  resave: true,
-  saveUninitialized: true
-}));
+// app.use(session({
+//   secret: "some secret goes here",
+//   resave: true,
+//   saveUninitialized: true
+// }));
 
 
 // app.use(passport.initialize());
@@ -68,7 +71,7 @@ app.locals.title = 'Dowell Systems - Employee Information';
 // CORS SETTINGS:
 app.use(cors({
   credentials: true,
-  origin: ['http://localhost:5000', 'http://localhost:3000']
+  origin: ['http://localhost:5000']
 }));
 
 
